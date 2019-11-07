a9d3b4c6b79caa2fc6c50f38472bf1f28594fa49
davidenkovic <davidenkovic100@gmail.com>
2019-11-07 21:13:38 +0100

    Done

---
+++ shell/src/shell/Shell.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shell;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Dave
+ */
+public class Shell {
+
+    public static void main(String[] args) throws IOException, InterruptedException{
+       System.out.print(">");
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+        String[] trimmedInput=input.trim().split("&");
+        
+        for (int i = 0; i < trimmedInput.length; i++) {
+            runShell(trimmedInput[i]);
+        }
+    }
+    static void runShell(String command) throws IOException,InterruptedException{
+        Process p=new ProcessBuilder().inheritIO().command("bash", "-c",command).start();
+        InputStream stream=p.getInputStream();
+        int x= stream.read();
+        
+        while(x != -1) {
+            System.out.print((char) x);
+            x = stream.read();
+        }
+        int exitCode=p.waitFor();
+        assert exitCode == 0;
+    }
+
+
+}
